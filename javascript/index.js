3ce08196bef5afcd59befaef83bc47339729b086
sherin17 <sherinthomasvc@gmail.com>
2022-09-06 09:01:58 +0530

    js

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+// console.log(document.getElementById("heading"));
+// console.log(document.getElementById("heading").textContent);
+// console.log(document.getElementById("heading").innerText);
+// console.log(document.getElementById("heading").innerHTML);
+// console.log(document.getElementById("dom").innerHTML);
+// console.log(document.getElementById("dom").innerText);
+// console.log(document.getElementById("dom").textContent);
+// console.log(document.getElementsByClassName("heading"));
+// console.log(document.getElementsByClassName("heading")[0]);
+let hold=document.getElementsByClassName("heading");
+for(let i=0;i<hold.length;i++){
+    hold[i].style.color="red";
+}
+// hold[0].style.color="red";
+// hold[1].style.fontFamily = "Calibri";
+
+// hold[1].innerHTML = "Meera";
+// console.log(hold[0].textContent);
